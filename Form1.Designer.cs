674a705b2a63f6e2b79ca4ccc613788832904367
erenkucukk <erenkucukk53@gmail.com>
2025-07-06 16:28:54 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace to_do_list
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.chkTasks = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // txtTask
+            // 
+            this.txtTask.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTask.Location = new System.Drawing.Point(213, 59);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(151, 34);
+            this.txtTask.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnAdd.Location = new System.Drawing.Point(409, 59);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(160, 34);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "+ Add Task";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Tomato;
+            this.btnDelete.Location = new System.Drawing.Point(271, 356);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(237, 44);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete Selected";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTitle.Location = new System.Drawing.Point(298, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(183, 41);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "TO-DO LIST";
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnClearAll.Location = new System.Drawing.Point(271, 394);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(237, 44);
+            this.btnClearAll.TabIndex = 7;
+            this.btnClearAll.Text = "Clear All";
+            this.btnClearAll.UseVisualStyleBackColor = false;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // chkTasks
+            // 
+            this.chkTasks.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.chkTasks.FormattingEnabled = true;
+            this.chkTasks.Location = new System.Drawing.Point(271, 112);
+            this.chkTasks.Name = "chkTasks";
+            this.chkTasks.Size = new System.Drawing.Size(237, 238);
+            this.chkTasks.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chkTasks);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtTask);
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.CheckedListBox chkTasks;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace to_do_list
+{
+    public partial class Form1 : Form
+    {
+        string filePath = "tasks.json";
+
+        public Form1()
+        {
+            InitializeComponent();
+            CustomizeDesign();
+        }
+
+        private void CustomizeDesign()
+        {
+            this.BackColor = Color.FromArgb(245, 245, 245);
+
+            lblTitle.Font = new Font("Segoe UI", 18, FontStyle.Bold);
+            lblTitle.ForeColor = Color.FromArgb(60, 60, 60);
+
+            txtTask.Font = new Font("Segoe UI", 12);
+            txtTask.Width = 250;
+
+            var buttons = new Button[] { btnAdd, btnDelete, btnClearAll };
+            Color[] colors = { Color.MediumSeaGreen, Color.Tomato, Color.SteelBlue };
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].BackColor = colors[i];
+                buttons[i].ForeColor = Color.White;
+                buttons[i].FlatStyle = FlatStyle.Flat;
+                buttons[i].FlatAppearance.BorderSize = 0;
+                buttons[i].Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            }
+
+            chkTasks.Font = new Font("Segoe UI", 11);
+            chkTasks.BackColor = Color.White;
+            chkTasks.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadTasksFromFile();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtTask.Text))
+            {
+                chkTasks.Items.Add(txtTask.Text);
+                txtTask.Clear();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            for (int i = chkTasks.CheckedIndices.Count - 1; i >= 0; i--)
+            {
+                chkTasks.Items.RemoveAt(chkTasks.CheckedIndices[i]);
+            }
+        }
+
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("All tasks will be deleted, are you sure?", "Yes", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                chkTasks.Items.Clear();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveTasksToFile();
+        }
+
+        private void SaveTasksToFile()
+        {
+            List<string> tasks = new List<string>();
+            foreach (var item in chkTasks.Items)
+            {
+                tasks.Add(item.ToString());
+            }
+
+            File.WriteAllText(filePath, JsonConvert.SerializeObject(tasks, Newtonsoft.Json.Formatting.Indented));
+        }
+
+        private void LoadTasksFromFile()
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                List<string> tasks = JsonConvert.DeserializeObject<List<string>>(json);
+                chkTasks.Items.Clear();
+                foreach (var task in tasks)
+                {
+                    chkTasks.Items.Add(task);
+                }
+            }
+        }
+    }
+}
